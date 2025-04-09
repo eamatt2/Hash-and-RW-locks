509e25a86e4f27c4ded714b8c3b043cd1f3b7ce2
JamesLove03 <jameslove2k22@gmail.com>
2025-04-08 20:31:42 -0400

    Now outputting correctly

--- chash.c
+++ chash.c
@@ -26,6 +26,8 @@ int main() {
         return 1;
     }
     printf("imalive");
+
+
     // Open output.txt for writing
     out = fopen("output.txt", "w");
     if (!out) {
@@ -34,31 +36,42 @@ int main() {
         return 1;
     }
 
+
     rwlock_init(&table_lock);  // Initialize lock
+    char buffer[50]; //holds first line
+    int dummy = 0;
+    char dummys[2];
+    int num_threads = 0;
+
+
+    if(sscanf(buffer,"%49[^,],%d,%d", dummys, &num_threads, &dummy))
+        fprintf(out, "Running %d threads\n", num_threads);
+
+        
 
     char line[128];
     while (fgets(line, sizeof(line), cmds)) {
         trim_newline(line);
-
+        printf("we loopin");
         char *cmd = strtok(line, ",");
         if (!cmd) continue;
 
-        if (strcmp(cmd, "INSERT") == 0) {
+        if (strcmp(cmd, "insert") == 0) {
             char *name = strtok(NULL, ",");
             char *salary_str = strtok(NULL, ",");
-
+            printf("Insert detected");
             if (name && salary_str) {
                 uint32_t salary = atoi(salary_str);
                 insert(name, salary);
                 fprintf(out, "INSERT,%s,%u\n", name, salary);
             }
-        } else if (strcmp(cmd, "DELETE") == 0) {
+        } else if (strcmp(cmd, "delete") == 0) {
             char *name = strtok(NULL, ",");
             if (name) {
                 delete(name);
                 fprintf(out, "DELETE,%s\n", name);
             }
-        } else if (strcmp(cmd, "SEARCH") == 0) {
+        } else if (strcmp(cmd, "search") == 0) {
             char *name = strtok(NULL, ",");
             if (name) {
                 hashRecord *res = search(name);
